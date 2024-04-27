2ba88fe20c413bb5710465636961152f2c93aac5
MohamedFaramawy <136634394+MohamedAymanHassan@users.noreply.github.com>
2024-04-27 09:47:52 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function redirect() {
+    window.location.href = "https://uiverse.io/vinodjangid07/splendid-bat-9";
+}
+function direct() {
+    window.location.href = "https://uiverse.io/MINOTAURO18/tasty-warthog-50";
+}
+function airect() {
+    window.location.href = "https://uiverse.io/MuhammadHasann/old-eagle-40";
+}
+function go() {
+    window.location.href = "product.html";
+}
